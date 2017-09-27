0dae917db5acb896edd8c3e8b1ce841f556d44f7
wwmin <wwei.min@163.com>
2017-09-27 12:49:02 +0800

    vue页面需要加入script标签否则会报错

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '../components/Hello.vue'
 
 const _import = require('./_import_' + process.env.NODE_ENV);
 Vue.use(Router);
@@ -15,7 +14,10 @@ import Layout from '../views/layout/Layout.vue'
  * meta : { role: ['admin'] }  will control the page role
  **/
 export const constantRouterMap = [
-  { path: '/login', component: _import('login/index'), hidden: true },
+  {path: '/login', component: _import('login/index'), hidden: true},
+  {path: '/authredirect', component: _import('login/authredirect'), hidden: true},
+  {path: '/404', component: _import('errorPage/404'), hidden: true},
+  {path: '/401', component: _import('errorPage/401'), hidden: true},
   {
     path: '/',
     component: Layout,
@@ -24,26 +26,14 @@ export const constantRouterMap = [
     hidden: true,
     children: [
       {path: 'dashboard', component: _import('dashboard/index')}
-      ]
-  },
-  {
-    path: '/404',
-    component: _import('errorPage/404'),
-    hidden: true
-  },
-  {
-    path: '/401',
-    component: _import('errorPage/401'),
-    hidden: true
-  },
-  {
-    path: '*',
-    name: 'errorPage',
-    meta: {
-      text: "首页"
-    },
-    redirect: '/404',
-    hidden: true
+    ]
+  }, {
+    path: '/introduction',
+    component: Layout,
+    redirect: '/introduction/index',
+    icon: 'people',
+    noDropdown: true,
+    children: [{path: 'index', component: _import('introduction/index')}]
   }
 ];
 export default new Router({
