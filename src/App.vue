ed7551bc5aacfb6223f1c1b67b1281a7e8b7395b
wwmin <wwei.min@163.com>
2017-09-27 10:54:32 +0800

    入口引入css

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,7 @@
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<style lang="scss">
+  @import '~normalize.css/normalize.css';// normalize.css 样式格式化
+  @import './styles/index.scss'; // 全局自定义的css样式
 </style>
