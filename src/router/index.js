2f6e67e87d8c713dea3efc11a6960264e60ef88a
wwmin <wwei.min@163.com>
2017-09-27 14:23:13 +0800

    完成图表

--- src/router/index.js
+++ src/router/index.js
@@ -80,5 +80,19 @@ export const asyncRouterMap = [
       {path: 'backtotop', component: _import('components/backToTop'), name: '返回顶部'}
     ]
   },
+  {
+    path: '/charts',
+    component: Layout,
+    redirect: '/charts/index',
+    name: '图表',
+    icon: 'chart',
+    children: [
+      { path: 'index', component: _import('charts/index'), name: '介绍' },
+      { path: 'keyboard', component: _import('charts/keyboard'), name: '键盘图表' },
+      { path: 'keyboard2', component: _import('charts/keyboard2'), name: '键盘图表2' },
+      { path: 'line', component: _import('charts/line'), name: '折线图' },
+      { path: 'mixchart', component: _import('charts/mixChart'), name: '混合图表' }
+    ]
+  },
   {path: '*', redirect: '/404', hidden: true}
 ];
