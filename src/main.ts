e3cb55948aa0fc6391075f6a6d6d97585040f6ba
wwmin <wwei.min@163.com>
2017-09-27 12:13:06 +0800

    引入filter后ts报7017错 [noImplicitAny:不允许隐式 any],添加"noImplicitAny": false解决

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as filters from './filters' // 全局filter
 import './icons/index.js' // icon
 import './mock'  // 该项目所有请求使用mockjs模拟
-
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 /* eslint-disable no-new */
